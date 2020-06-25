8340542921db15fde0bf93cf79010c36499def22
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:28:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,7 @@ class Student
     new_id = row[0]
     new_name = row[1]
     new_grade = row[2]
-    new_student = Student.new(new_id, new_name, new_grade)
+    new_student = Student.new
     new_student
   end
 

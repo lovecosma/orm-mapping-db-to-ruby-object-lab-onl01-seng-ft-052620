85046511e3692bce768120979108600fc6f8da13
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:36:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,6 +19,10 @@ class Student
       WHERE grade = ?
       LIMIT 1
     SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+     self.new_from_db(row)
+     end.first
   end
 
   def self.find_by_name(name)

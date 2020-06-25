9789d78031017cc27fb74f6843b74ed1250e38cc
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:53:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,11 +29,6 @@ class Student
     SQL
 
     row = DB[:conn].execute(sql)
-    new_student = Student.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
   end
 
   def self.all_students_in_grade_9

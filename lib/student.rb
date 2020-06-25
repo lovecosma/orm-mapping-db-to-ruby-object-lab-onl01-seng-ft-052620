caeafe58344943806f1a8a93b33648b5e37e3dda
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:36:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,12 @@ class Student
   end
 
   def self.all_students_in_grade_9
-  
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
   end
 
   def self.find_by_name(name)

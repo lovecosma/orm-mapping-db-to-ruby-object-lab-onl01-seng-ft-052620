8ac0fd604f62c6b9939c07b00d169b01b60a8d5a
Matthew Williams <williamsmatthew333@gmail.com>
2020-06-25 19:37:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,14 +16,14 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = ?
+      WHERE grade = 9
       LIMIT 1
     SQL
 
     DB[:conn].execute(sql, grade).map do |row|
      self.new_from_db(row)
      end.first
-     
+
   end
 
   def self.find_by_name(name)
